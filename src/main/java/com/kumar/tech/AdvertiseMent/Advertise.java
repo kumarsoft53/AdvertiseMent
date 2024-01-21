ea70df9e81a403a0b981855a4a82c605b0bc59be
kumarsoft53 <kumar.soft53@gmail.com>
2024-01-21 13:24:52 +0530

    2nd commit

--- src/main/java/com/kumar/tech/AdvertiseMent/Advertise.java
+++ src/main/java/com/kumar/tech/AdvertiseMent/Advertise.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Advertise {
-    @GetMapping("notice")
+    @GetMapping("/notice")
     public static String display(){
         return "Please contact me for advertisementt";
 
